279d864c7b51f3cfc245cb1eea3a304078972127
Arsenault_Zachary <e1937017@cmaisonneuve.qc.ca>
2022-02-17 10:36:31 -0500

    premiercommit

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+<main class="principal">
+        <h2 class="formation__titre">-------front-page.php---------</h2>
+            <?php if (have_posts()):the_post()?>
+                    <?php
+                        the_title();
+                        the_content();
+                    ?>
+            <?php endif ?>
+        </div>
+    </section>
+</main>
+<?php get_footer() ?>
